6b9ea1a9c60b882586dc431f32f15f969c7353b7
JohVitor <jvagardizan@gmail.com>
2021-08-12 15:26:23 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+
+import MostraVoltas from './MostraVoltas'
+import MostraTempo from './MostraTempo'
+import Button from './Button'
+
+import './styles.css'
+
 
 function App() {
+
+  const [numeroVoltas, setNumeroVoltas] = useState(0)
+  const [correr, setCorrer] = useState(false)
+  const [tempo, setTempo] = useState(0)
+
+  useEffect(()=> {
+    let timer = null
+    if(correr) {
+      timer = setInterval(()=> {
+        setTempo((antigo) => antigo + 1)
+      }, 1000)
+    }
+    return () => {
+      if(timer) { // valor que é convertido para verdadeiro
+        clearInterval(timer)
+      }
+    }
+  }, [correr])
+
+  const incrementarVoltas = () => {
+    setNumeroVoltas(numeroVoltas + 1)
+  }
+  const decrementarVoltas = () => {
+    setNumeroVoltas(numeroVoltas - 1)
+  }
+
+
+  let [btnCorrer, setBtnCorrer] = useState('Iniciar')
+
+  const toggleCorrer = () => {
+    setCorrer(!correr)
+    if(correr === false) {
+      setBtnCorrer(btnCorrer = 'Pausar')
+    } else {
+      setBtnCorrer(btnCorrer = 'Iniciar')
+    }
+  }
+  
+  const reset = () => {
+    setNumeroVoltas(0)
+    setTempo(0)
+    setBtnCorrer(btnCorrer = 'Iniciar')
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <MostraVoltas voltas={numeroVoltas} />
+      <div className="container-btn">
+        <Button onClick={incrementarVoltas} text='+' />
+        <Button onClick={decrementarVoltas} text='-' />
+      </div>
+      
+      {
+        numeroVoltas > 0 &&
+        <MostraTempo tempo = {Math.round(tempo / numeroVoltas)} />  
+      }
+      <div className="container-btn">
+        <Button onClick={toggleCorrer} text={btnCorrer} />
+        <Button onClick={reset} text='Reiniciar' />
+      </div>
     </div>
   );
 }
---
+++ src/MostraVoltas.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MostraVoltas = (props) => {
+    return(
+      <p>
+        {props.voltas}
+        <br />
+        Voltas
+      </p>
+    )
+}
+
+export default MostraVoltas
